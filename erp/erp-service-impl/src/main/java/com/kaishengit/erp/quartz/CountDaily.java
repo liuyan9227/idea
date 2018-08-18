ea0cda42947cfbad2a84b82ead5c58732aa056a9
liuyan9227 <liuyan9227@163.com>
2018-08-18 12:47:43 +0800

    事务调度

---
+++ erp/erp-service-impl/src/main/java/com/kaishengit/erp/quartz/CountDaily.java
@@ -0,0 +1,12 @@
+package com.kaishengit.erp.quartz;
+
+/**
+ * @author liuyan
+ * @date 2018/8/17
+ */
+public class CountDaily {
+
+    public void execute (){
+
+    }
+}
---
+++ scheduletask/src/main/java/com/kaishengit/job/MySpringQuartzJob.java
@@ -0,0 +1,8 @@
+package com.kaishengit.job;
+
+/**
+ * @author liuyan
+ * @date 2018/8/17
+ */
+public class MySpringQuartzJob {
+}
