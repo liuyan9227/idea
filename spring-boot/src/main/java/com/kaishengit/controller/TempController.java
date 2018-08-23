f9176a267d6e02c776e46284bc460e48ac116fc9
liuyan9227 <liuyan9227@163.com>
2018-08-23 14:34:50 +0800

    springboot

--- spring-boot/src/main/java/com/kaishengit/controller/TempController.java
+++ spring-boot/src/main/java/com/kaishengit/controller/TempController.java
@@ -1,38 +1,35 @@
 package com.kaishengit.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.kaishengit.entity.Car;
 import com.kaishengit.entity.CarExample;
 import com.kaishengit.mapper.CarMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * @author liuyan
- * @date 2018/8/22
+ * @date 2018/8/23
  */
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/car")
+public class TempController {
 
     @Autowired
     private CarMapper carMapper;
 
     @GetMapping
-    public String home(Model model){
-        List<String> nameList = Arrays.asList("max,mini");
-
+    @ResponseBody
+    public List<Car> cars(){
+        PageHelper.startPage(1, 3);
         List<Car> carList = carMapper.selectByExample(new CarExample());
-        model.addAttribute("carList", carList);
-        model.addAttribute("age", 26);
-        model.addAttribute("userName", "mini");
-        model.addAttribute("name", "max");
-        model.addAttribute("nameList", nameList);
-        return "home";
+        PageInfo page = new PageInfo(carList);
+        return carList;
     }
 }
---
+++ spring-boot/src/main/java/com/kaishengit/mapper/CarMapper.java
@@ -0,0 +1,33 @@
+package com.kaishengit.mapper;
+
+import com.kaishengit.entity.Car;
+import com.kaishengit.entity.CarExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface CarMapper {
+    long countByExample(CarExample example);
+
+    int deleteByExample(CarExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Car record);
+
+    int insertSelective(Car record);
+
+    List<Car> selectByExample(CarExample example);
+
+    Car selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Car record, @Param("example") CarExample example);
+
+    int updateByExample(@Param("record") Car record, @Param("example") CarExample example);
+
+    int updateByPrimaryKeySelective(Car record);
+
+    int updateByPrimaryKey(Car record);
+}
